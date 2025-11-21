6e68130f02753b2505430197d1d6494eefa81b0a
axemonkey <hi+github@clivemurray.com>
2025-11-21 10:34:51 +0000

    singlequotes in prettierrc

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,11 @@
-import { months, days } from "./modules/data.js";
+import { months, days } from './modules/data.js';
 import {
 	getDaysInMonth,
 	getTheDay,
 	lz,
 	getURLParams,
-} from "./modules/tools.js";
-import { getBankHolidays } from "./modules/holidays.js";
+} from './modules/tools.js';
+import { getBankHolidays } from './modules/holidays.js';
 
 /*
 
@@ -27,14 +27,14 @@ TO DONE:
 */
 
 const init = () => {
-	console.log("go");
+	console.log('go');
 	let year;
 	let month;
 
 	const qp = getURLParams();
-	if (qp.get("year") && qp.get("month")) {
-		year = Number(qp.get("year"));
-		month = Number(qp.get("month"));
+	if (qp.get('year') && qp.get('month')) {
+		year = Number(qp.get('year'));
+		month = Number(qp.get('month'));
 	} else {
 		const now = new Date();
 		month = now.getMonth();
@@ -51,9 +51,9 @@ const createNavButtons = (year, month) => {
 	const nextMonth = month === 11 ? 0 : month + 1;
 	const nextYear = month === 11 ? year + 1 : year;
 
-	document.querySelector(".prevMonth").innerHTML =
+	document.querySelector('.prevMonth').innerHTML =
 		`<a href="/?year=${prevYear}&month=${prevMonth}">&lt;&lt;</a>`;
-	document.querySelector(".nextMonth").innerHTML =
+	document.querySelector('.nextMonth').innerHTML =
 		`<a href="/?year=${nextYear}&month=${nextMonth}">&gt;&gt;</a>`;
 };
 
@@ -69,14 +69,14 @@ const createMonth = (year, month) => {
 	// console.log(`today: ${dayStr}`);
 	console.log(`first day of month: ${firstDayStr}`);
 
-	document.querySelector(".monthTitle").textContent = `${monthStr} ${year}`;
+	document.querySelector('.monthTitle').textContent = `${monthStr} ${year}`;
 
-	const monthHeaderDiv = document.querySelector(".monthHeader");
-	const monthDiv = document.querySelector(".monthBody");
+	const monthHeaderDiv = document.querySelector('.monthHeader');
+	const monthDiv = document.querySelector('.monthBody');
 	monthDiv.replaceChildren(); // empty the month div
 
 	for (const day of days) {
-		const dayDiv = document.createElement("div");
+		const dayDiv = document.createElement('div');
 		dayDiv.innerHTML = day.slice(0, 3);
 		dayDiv.className = `dayHeader day-${day}`;
 		monthHeaderDiv.append(dayDiv);
@@ -84,23 +84,23 @@ const createMonth = (year, month) => {
 
 	let week = 1;
 	for (let currDate = 1; currDate <= monthLength; currDate++) {
-		const dateDiv = document.createElement("div");
+		const dateDiv = document.createElement('div');
 		dateDiv.innerHTML = `<p>${currDate}</p>`;
 		const theDay = getTheDay(year, month, currDate - 1);
 		dateDiv.className = `day day${currDate} day-${days[
 			theDay
 		].toLowerCase()} week${week}`;
 		if (theDay >= 5) {
-			dateDiv.classList.add("weekend");
+			dateDiv.classList.add('weekend');
 		}
 		if (currDate <= 7) {
-			dateDiv.classList.add("first7");
+			dateDiv.classList.add('first7');
 		}
 		if (currDate > monthLength - 7) {
-			dateDiv.classList.add("last7");
+			dateDiv.classList.add('last7');
 		}
 		if (currDate === monthLength) {
-			dateDiv.classList.add("last");
+			dateDiv.classList.add('last');
 		}
 		dateDiv.dataset.date = `${year}-${lz(month + 1)}-${lz(currDate)}`;
 		monthDiv.append(dateDiv);
@@ -129,14 +129,14 @@ const highlightBankHolidays = (bhArray, year, month, monthLength) => {
 			if (bh.date === checkDate) {
 				console.log(`BH found! ${checkDate}`);
 				const bhDiv = document.querySelector(`[data-date="${checkDate}"]`);
-				bhDiv.classList.add("bh");
-				const bhLabel = document.createElement("div");
-				bhLabel.classList.add("bhLabel");
-				bhLabel.textContent = "Bank Holiday";
+				bhDiv.classList.add('bh');
+				const bhLabel = document.createElement('div');
+				bhLabel.classList.add('bhLabel');
+				bhLabel.textContent = 'Bank Holiday';
 				bhDiv.append(bhLabel);
 			}
 		}
 	}
 };
 
-window.addEventListener("load", init);
+window.addEventListener('load', init);
--- src/assets/js/modules/holidays.js
+++ src/assets/js/modules/holidays.js
@@ -1,9 +1,9 @@
 const getBankHolidays = async (year, month) => {
 	console.log(`get bank holidays for: ${year}, ${month}`);
 
-	const response = await fetch("https://www.gov.uk/bank-holidays.json");
+	const response = await fetch('https://www.gov.uk/bank-holidays.json');
 	const bh = await response.json();
-	const selectedBH = bh["england-and-wales"].events;
+	const selectedBH = bh['england-and-wales'].events;
 
 	return selectedBH;
 };
